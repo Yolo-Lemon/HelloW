47d1d5249c6bd131c406b1319f7009271a5acdde
Yolo-Lemon <sveyzman@gmail.com>
2025-01-02 16:34:11 +0300

    Update Hello.java
    Исправлено форматирование кода

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 package Hello;
 
 public class Hello {
-public static void main(String[] args) {
-System.out.println("Hello World\\!&*");
-}}
-
+    public static void main(String[] args) {
+        System.out.println("Hello World\\!&*");
+    }
+}
